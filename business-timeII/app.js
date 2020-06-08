dafccb698420f7daa1837f2c43d6e4926a771ae1
Emily Parker <emilyarparker@gmail.com>
2020-06-08 00:16:30 -0600

    almost there

--- business-timeII/app.js
+++ business-timeII/app.js
@@ -1,9 +1,23 @@
-const buttOne = document.getElementById("butt1")
+const buttOne = document.getElementById("butt1");
 
-buttOne.addEventListener("click, buttonClicked")
+buttOne.addEventListener("click, buttonClicked");
 
-function buttonClicked(){
-    console.log("hello")
+function buttonClicked() {
+    console.log("hello");
 }
 
+const buttTwo = document.getElementById("butt2");
 
+buttTwo.addEventListener("click, buttonWorks");
+
+function buttonWorked() {
+    console.log("on the way to contact and story");
+}
+
+const buttThree = document.getElementById("butt3");
+
+buttThree.addEventListener("click, buttonPressed");
+
+function buttonPressed() {
+    console.log("on the way to contact & story");
+}
