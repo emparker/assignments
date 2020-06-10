96e2ce30f46b6ed6663883e1b0c0ebb0874bddd8
Emily Parker <emilyarparker@gmail.com>
2020-06-10 10:56:18 -0600

    NodeCalculator

---
+++ NodeCalculator/app.js
@@ -0,0 +1,56 @@
+const readline = require("readline-sync");
+
+const userName = readline.question("May I have your name? ");
+
+// console.log('Hi ' + userName + ' !' + 'Welcome to ')
+
+console.log(`Hi ${userName}! Welcome to Emily's Node Caluclator`);
+
+const num1 = readline.question("Please enter first number: ");
+
+const num2 = readline.question("Please enter second number: ");
+
+const operations = ["Addition", "Subtraction", "Multiplication", "Division"];
+// operations[0] = num1 + num2
+// operations[1] = num1 - num2
+// operations[2] = num1 * num2
+// operations[3] = num1 / num2
+
+const index = readline.keyInSelect(operations,"Which operation would you like to perform?");
+
+
+
+function add(num1, num2){
+    return num1 + num2
+}
+
+function subtract(num1, num2){
+    return num1 - num2
+}
+
+function multiply(num1, num2){
+   return num1 * num2
+}
+
+function divide(num1, num2){
+    return num1 / num2
+}
+// for (let i = 1; i <= operations.length; i++){
+//     if (i == operations[0])
+// }
+let result = 0
+switch (operations[index]) {
+    
+  case "Addition": add()
+    break;
+  case "Subtraction": subtract()
+    break;
+  case "Multiplication": multiply()
+    break;
+   case "Division": divide()
+    break;
+  default:
+    console.log ("sorry you can't do that")
+}
+
+// console.log(`Sweet, your answer is ${result}. Great job!`);
