b7f503f380e4fb2899b95b397d516c78fe54aad8
Emily Parker <emilyarparker@gmail.com>
2020-06-13 10:21:28 -0600

    escape-room progress

--- escape-room/app.js
+++ escape-room/app.js
@@ -1,41 +1,62 @@
+// Tell the user their options:
 
+// Put hand in hole
+// Find the key, or
+// Open the door
+// They can't open the door unless they find the key first.
+// They die if they put their hand in the hole.
 
+// JavaScript tools that may help
+// while loop
+// variables
+// booleans
+// if/else if/else OR switch statement
+// readline-sync (syntax for installing and using below)
 
+const readline = require("readline-sync");
 
-//const buttonHole = document.getElementById("hole")
-const buttonKey = document.getElementById("key")
-const buttonDoor = document.getElementById("door") 
- 
+const name = readline.question("What is your name? ");
+    console.log(`hi ${name}! Welcome to the ESCAPE ROOM.`);
 
+const usersPick = ["Put your hand in the hole", "Find the key", "Open the door"];
 
-//buttonHole.addEventListener("click", buttonHolePressed)
-buttonKey.addEventListener("click", buttonKeyPressed)
-buttonDoor.addEventListener("click", buttonDoorPressed)
+const index = readline.keyInSelect(usersPick, `Hi ${name}! You must pick one of these three options, what will you choose?`);
 
+function handHole(usersPick){
+    return usersPick= false
+}
+function findKey(usersPick, index){
+    return usersPick= true
+}
+ function openDoor(usersPick, index){
+     return usersPick= true
+ }
+    
+let result = null;
 
-Function;buttonHolePressed("click", true);{
-    document.getElementById("hole").innerHTML = "YOU DONEZO FOO";
+while(true){
+    switch (usersPick[index]){
+    case "Put your hand in the hole": result= handHole()
+        console.log("NOOO you fool! GAME OVER") 
+        break; 
+    case "Open the door": result= openDoor()
+        console.log("First you must find the key to unlock the door.")
+    case "Find the key": result= findKey()
+        console.log("Congrats, you found the key and can now unlock the door!")
+        break;
+    default:
+        console.log("Please pick the option 1, 2 or 3")
+    }
+    console.log(`Wow, ${name} what a game, well done. Please play again soon!`);
 }
 
-function buttonKeyPressed(){
-    console.log("Congrats, you've escaped!")
-}
-
-function buttonDoorPressed(){
-    console.log("game over")
-}
-
-buttonKeyPressed === true;
-//buttonHolePressed === false;
-buttonDoorPressed === false;
-
-if (buttonKeyPressed === true){
-    console.log("You win!")
-} else if (buttonHolePressed){
-    console.log("NOOOOOO")
-} else if (buttonDoorPressed){
-    console.log("The door is locked, game over")
-}
+// if (key === true){
+//     console.log("You win!")
+// } else if (hole === false){
+//     console.log("NOOOOOO")
+// } else if (door){
+//     console.log("The door is locked, game over")
+// }
 
 
 
