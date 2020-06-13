0deb61645b3795417f2e19d169574d8485bcf205
Emily Parker <emilyarparker@gmail.com>
2020-06-13 10:48:23 -0600

    escape-room working, but not done

--- escape-room/app.js
+++ escape-room/app.js
@@ -20,36 +20,39 @@ const name = readline.question("What is your name? ");
 
 const usersPick = ["Put your hand in the hole", "Find the key", "Open the door"];
 
-const index = readline.keyInSelect(usersPick, `Hi ${name}! You must pick one of these three options, what will you choose?`);
+const index = readline.keyInSelect(usersPick, `OK ${name}, you must pick one of these three options, what will you choose?`);
 
 function handHole(usersPick){
-    return usersPick= false
-}
-function findKey(usersPick, index){
     return usersPick= true
 }
- function openDoor(usersPick, index){
-     return usersPick= true
+function findKey(usersPick){
+    return usersPick= true
+}
+ function openDoor(usersPick){
+    return usersPick= true
  }
     
 let result = null;
 
 while(true){
     switch (usersPick[index]){
-    case "Put your hand in the hole": result= handHole()
+    case "Put your hand in the hole": result= handHole() 
         console.log("NOOO you fool! GAME OVER") 
         break; 
     case "Open the door": result= openDoor()
         console.log("First you must find the key to unlock the door.")
+        break;
     case "Find the key": result= findKey()
         console.log("Congrats, you found the key and can now unlock the door!")
         break;
     default:
         console.log("Please pick the option 1, 2 or 3")
     }
-    console.log(`Wow, ${name} what a game, well done. Please play again soon!`);
+    return result;
 }
 
+
+// console.log(`Wow, ${name} what a game, well done. Please play again soon!`);
 // if (key === true){
 //     console.log("You win!")
 // } else if (hole === false){
