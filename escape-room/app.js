6f3aa9dc4ffac7b82893989d84206acf111afc1f
Emily Parker <emilyarparker@gmail.com>
2020-06-16 10:42:06 -0600

    finished and working game

--- escape-room/app.js
+++ escape-room/app.js
@@ -12,44 +12,51 @@
 // booleans
 // if/else if/else OR switch statement
 // readline-sync (syntax for installing and using below)
-
 const readline = require("readline-sync");
-
 const name = readline.question("What is your name? ");
     console.log(`hi ${name}! Welcome to the ESCAPE ROOM.`);
 
-const usersPick = ["Put your hand in the hole", "Find the key", "Open the door"];
+let gamePlayOn = true
+let hasKey = false
 
-const index = readline.keyInSelect(usersPick, `OK ${name}, you must pick one of these three options, what will you choose?`);
-
-function handHole(usersPick){
-    return usersPick= true
-}
-function findKey(usersPick){
-    return usersPick= true
-}
- function openDoor(usersPick){
-    return usersPick= true
- }
+while(gamePlayOn === true){
     
-let result = null;
-
-while(true){
+    const usersPick = ["Put your hand in the hole", "Find the key", "Open the door"];
+    const index = readline.keyInSelect(usersPick, `OK ${name}, you must pick one of these three options, what will you choose?`);
+        
     switch (usersPick[index]){
-    case "Put your hand in the hole": result= handHole() 
+    
+    case "Put your hand in the hole": 
         console.log("NOOO you fool! GAME OVER") 
-        break; 
-    case "Open the door": result= openDoor()
+        gamePlayOn = false
+        return; 
+    case "Find the key": 
+        hasKey = true
+        console.log("Congrats, you found the key and can now unlock the door! You're almost there! Pick one more option")
+        console.log(hasKey)
+        break;
+    case "Open the door": 
+    
+    if (hasKey === false){
+        console.log(hasKey)
         console.log("First you must find the key to unlock the door.")
         break;
-    case "Find the key": result= findKey()
-        console.log("Congrats, you found the key and can now unlock the door!")
+    } else {
+        console.log("CONGRATS you've escaped!")
+        console.log(hasKey)
+        return
+        
+    } 
         break;
     default:
         console.log("Please pick the option 1, 2 or 3")
+        } 
+        //return
     }
-    return result;
-}
+    
+
+
+    
 
 
 // console.log(`Wow, ${name} what a game, well done. Please play again soon!`);
