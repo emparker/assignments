e39d103c817d6bdc20f7b93ca4cafeddb6e37cad
Emily Parker <emilyarparker@gmail.com>
2020-06-27 22:44:02 -0600

    trying to delete added list items

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -12,12 +12,21 @@ form.addEventListener("submit", function(event){
     document.getElementById("list").append(li)
 
     const div = document.createElement("div")
-    const button = document.createElement("button")
-    const xButton = ("X")//(this is only representing the button tag?)
+    let button = document.createElement("button")
+    const xButton = ("X")
 
     button.append(xButton)
     div.append(button)
     li.append(div)
     //console.log(xButton)
+    // button = null
+
+
+function removeItem(){
+    document.addedItem.div.innerHTML = ""  // remove? //innerHTML<-??
+}
+
+document.button.addEventListener("click", removeItem)//why is button undefined?
 
 })
+// maybe if statement?
