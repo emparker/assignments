c41b1ed28388e5795ea0283d25c255c8983da175
Emily Parker <emilyarparker@gmail.com>
2020-06-29 11:09:27 -0600

    completed style if want

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -4,34 +4,26 @@ const form = document.addItem
 form.addEventListener("submit", function(event){
     event.preventDefault()
 
-    let addedItem = form.title.value
+    const addedItem = form.title.value
     form.title.value = ""
 
     const li = document.createElement("li")
     li.textContent = addedItem
     document.getElementById("list").append(li)
 
-    let div = document.createElement("div")
-    let button = document.createElement("button")  // let or const?
-    const xButton = ("X")
-
-    button.append(xButton)
-    div.append(button)
-    li.append(div)
-    //console.log(xButton)
-
-
- 
     
-        function removeItem(e){
-            let addedItem = e.target.div
-            HTMLFormElement.target = addedItem
-            // let div = null
-            div.remove(list.div)
-    }  
-    button.addEventListener("click", removeItem)
-
+    const button = document.createElement("button") 
+    const xButton = ("X")
+    button.style.display="block"
+    button.append(xButton)
+    li.append(button)
+    
+    button.addEventListener("click", ()=>{
+        li.remove()
+    })
+    
 })
+
 // another attempted method:
    // button = null
     // function removeItem(button, div){
