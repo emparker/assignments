3acc9677620d8c1019eaafeb79a89ddf193318a9
Emily Parker <emilyarparker@gmail.com>
2020-06-28 21:52:21 -0600

    x button will delete but not added item

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -4,29 +4,40 @@ const form = document.addItem
 form.addEventListener("submit", function(event){
     event.preventDefault()
 
-    const addedItem = form.title.value
+    let addedItem = form.title.value
     form.title.value = ""
 
     const li = document.createElement("li")
     li.textContent = addedItem
     document.getElementById("list").append(li)
 
-    const div = document.createElement("div")
-    let button = document.createElement("button")
+    let div = document.createElement("div")
+    let button = document.createElement("button")  // let or const?
     const xButton = ("X")
 
     button.append(xButton)
     div.append(button)
     li.append(div)
     //console.log(xButton)
-    // button = null
 
 
-function removeItem(){
-    document.addedItem.div.innerHTML = ""  // remove? //innerHTML<-??
-}
-
-document.button.addEventListener("click", removeItem)//why is button undefined?
+ 
+    
+        function removeItem(e){
+            let addedItem = e.target.div
+            HTMLFormElement.target = addedItem
+            // let div = null
+            div.remove(list.div)
+    }  
+    button.addEventListener("click", removeItem)
 
 })
-// maybe if statement?
+// another attempted method:
+   // button = null
+    // function removeItem(button, div){
+    //     if (button == div){
+    //         div.target.removeChild(button)  //!!!!
+    //     }   else{
+    //         return false
+    //     }
+        // button.addEventListener("click", removeItem)//why is button undefined?
