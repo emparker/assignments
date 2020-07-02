ef0ebaeefa187bfb623dcf65c1b55e630cea980e
Emily Parker <emilyarparker@gmail.com>
2020-07-01 19:30:09 -0600

    going for extra cred with some toggling but need help

--- loops-and-arrays-practice/app.js
+++ loops-and-arrays-practice/app.js
@@ -73,6 +73,21 @@ Madeline is old enough. SHE'S good to see Mad Max Fury Road.
 
 // Sample Arrays:
 
-[2, 5, 435, 4, 3] // "The light is on"
-[1, 1, 1, 1, 3]   // "The light is on"
-[9, 3, 4, 2]      // "The light is off"
+const arrays= [
+[2, 5, 435, 4, 3],  // "The light is on"
+[1, 1, 1, 1, 3],    // "The light is on"
+[9, 3, 4, 2]       // "The light is off"
+]      
+for ( i = 0 ; i <= arrays.length; i++ ){
+    //console.log(arrays.length)
+    arrays.reduce(myFunc)
+    function myFunc(total, num){
+        return total + num
+    }
+}
+/*numbers.reduce(myFunc);
+
+function myFunc(total, num) {
+return total - num;
+}
+*/
