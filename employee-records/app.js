cc1fc66381ac0bb16fbe9b409c83d7d023252394
Emily Parker <emilyarparker@gmail.com>
2020-07-13 16:43:26 -0600

    employee records complete

--- employee-records/app.js
+++ employee-records/app.js
@@ -9,7 +9,13 @@ function Employee(name, jobTitle, salary){
         this.status = work
     }
     this.printEmployeeForm = function(){
-        console.log("")
+        console.log(`
+            Name: ${this.name},
+            Job Title: ${this.jobTitle},
+            Salary: ${this.salary},
+            Status: ${this.status}
+        `)
+        //console.log('Name: ' + this.name + '\nJob Title: ' + this.jobTitle)
     }
 
 }
@@ -20,8 +26,14 @@ const employee3 = new Employee("Barney Rubble", "Welder", "40,000/yr")
 
 employee3.changeStatus("Contract")
 
-const emp1 = employee1.printEmployeeForm()
-console.log(employees.push(emp1))
+employees.push(employee1, employee2, employee3)
+
+for(let i = 0; i < employees.length; i++){
+    employees[i].printEmployeeForm()
+}
+
+//console.log(emp1)
+//console.log(employees.push(emp1))
 
 // employee2.printEmployeeForm()
 // employee3.printEmployeeForm()
@@ -31,7 +43,7 @@ console.log(employees.push(emp1))
 // }
 // printEmployeeForm()
 
-console.log(employee1)
-console.log(employee2)
-console.log(employee3)
+// console.log(employee1)
+// console.log(employee2)
+// console.log(employee3)
 
