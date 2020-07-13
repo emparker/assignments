5f0c2d6867fe73d22499a34d27fe36752d7ef715
Emily Parker <emilyarparker@gmail.com>
2020-07-12 22:34:50 -0600

    working on printEmployeeFormmethod inside construtor function

--- employee-records/app.js
+++ employee-records/app.js
@@ -5,20 +5,33 @@ function Employee(name, jobTitle, salary){
     this.jobTitle = jobTitle
     this.salary = salary
     this.status = "Full Time"
+    this.changeStatus = function (work){
+        this.status = work
+    }
+    this.printEmployeeForm = function(){
+        console.log("")
+    }
+
 }
 
 const employee1 = new Employee("Whilma Flinstone", "Teacher", "100,000/yr")
 const employee2 = new Employee("Fred Flinstone", "Forman", "50,000/yr")
 const employee3 = new Employee("Barney Rubble", "Welder", "40,000/yr")
 
-Employee.prototype.printEmployeeForm = function(){
-    // const emp1 = this.push(employees) 
-    // console.log(emp1)
-    //return this.name.jobTitle.salary.status
+employee3.changeStatus("Contract")
 
-}
+const emp1 = employee1.printEmployeeForm()
+console.log(employees.push(emp1))
 
-//console.log(employee1.status)
-// const emp1 = employees.push(employee1)
-// console.log(emp1)
+// employee2.printEmployeeForm()
+// employee3.printEmployeeForm()
+
+// Employee.printEmployeeForm = function(){
+//     return employees.push(Employee)
+// }
+// printEmployeeForm()
+
+console.log(employee1)
+console.log(employee2)
+console.log(employee3)
 
