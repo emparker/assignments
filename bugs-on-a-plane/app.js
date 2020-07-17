affa0cd47c59c147abd84b085d67302d4dd45907
Emily Parker <emilyarparker@gmail.com>
2020-07-17 10:28:47 -0600

    bugs on a plane completed

--- bugs-on-a-plane/app.js
+++ bugs-on-a-plane/app.js
@@ -1,30 +1,25 @@
-var form = document.getElementById('arline-form');   //null??
-var submit = document.getElementById('submit');
+var form = document.getElementById("airlineForm");   //null??
+var submit = document.getElementById("submit");
 var query = document.querySelector;
 
-function formAlert() {
-    var firstName = form.getElementsByClassName("firstName").value;  //just had 'elements["name"].value
-    var lastName = form.getElementsByClassName("lastName").value;       //form is null??
-    var age = form.getElementsByClassName("age").value;
-    var gender = form.getElementsByClassName("gender").value;
-    var location = form.getElementsByClassName("travel-location").value;
-    var diet = {};
-    if (form.getElementsByClassName('vegan').checked) {
-        const veg = document.getElementById("vegan").value;
-        diet.pop(veg)
-    }
-    if (form.getElementsByClassName('gluten').checked) {
-        const glute = document.getElementById('gluten').value;
-        diet.push(glute)
-    }
-    if (form.getElementsByClassName('paleo').checked) {
-        const pal = document.getElementById('paleo').value;
-        diet.push(pal)
+submit.addEventListener("click", function formAlert(){
+    const firstName = form.elements["firstName"].value;  //just had 'elements["name"].value
+    const lastName = form.elements["lastName"].value;       //form is null??
+    const age = form.elements["age"].value;
+    const gender = form.elements["gender"].value;
+    const location = form.elements["travelLocation"].value;
+    const diet = [];
+    if (form.elements["vegan"].checked) {
+        diet.push(document.getElementById("vegan").value);
+        
+    } else if (form.elements["gluten"].checked) {
+        diet.push(document.getElementById("gluten").value);
+        
+    } else if (form.elements["paleo"].checked) {
+        diet.push(document.getElementById("paleo").value);
     }
 
 
     alert("First Name: " + firstName + "\nLast Name: " + lastName + "\nAge: " + age + "\nGender: " + gender + "\nTravel Location: " + location + "\nDiet: " + diet + "\nAwesome, now if you die, it won't be an accident..");
-}
 
-
-submit.addEventListener("click", formAlert);
+})
