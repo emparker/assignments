7c978831b3aa340ab7f44762f6927f3801f6fb18
Emily Parker <emilyarparker@gmail.com>
2020-07-18 23:27:21 -0600

    one more problem to solve

--- problem-solving-practice/app.js
+++ problem-solving-practice/app.js
@@ -1,43 +1,22 @@
+//*
 //Write a function that takes an array of numbers and returns the largest (without using Math.max())
+//largest() // => 8
 
-//largest([3, 5, 2, 8, 1]) // => 8
+function largest(arr){
+    const largestNum = []; // if [] logs 1 if [0] logs 2??? AND WHY VAR?!
+    for (var i = 0; i < arr.length; i++){
+        for (var j = 0; j < arr[i].length; j++){
+            if (arr[i][j] > largestNum[i]){
+                largestNum[i] = arr[i][j];
+            }
+        }
+    }
+    return largestNum.push(largestNum[i]);
+}
+const showMe8 = largest([3, 5, 2, 8, 1])
+console.log(showMe8)
 
-// function largest(number){
-//     for (let i = 1; i < arr.length; i++){
-//         const arr = number.split("").map(largest);
-//         const highNumber = arr[0];
-//             if (arr[i] > highNumber){
-//             highNumber = arr[i];
-//             }
-//     }
-//     return highNumber;
-// }
-// console.log(largest([3, 5, 2, 8, 1]))
-// function largest(arr){
-//     arr = []
-//     for (let i = 0; i < arr.length; i++){
-//         const numbers = arr[i].toString();
-//         numbers.map(Number);
-//         const highNumber = arr[0];
-//             if (numbers < highNumber){
-//                 arr.push(numbers)
-//             }
-
-//     }
-//     return highNumber
-// }
-// console.log(largest([3, 5, 2, 8, 1]))
-// function largest(arr){
-//     const highNumber = arr[0];
-//     for (let i = 0; i < arr.length; i++){
-//         if (arr[i] > highNumber){
-//             return highNumber;
-//         }
-//     }
-//     //return highNumber;
-// }
-//console.log(largest([3, 5, 2, 8, 1]))
-
+//*
 //*
 // Write a function that takes an array of words and a character and returns 
 // each word that has that character present.
@@ -56,4 +35,24 @@ function lettersWithStrings( anArray, aString ){
 const newVar = lettersWithStrings(["#3", "$$$", "C%4!", "Hey!"], "!")
 console.log(newVar)
 //*
+//*
+// Write a function that takes a num1 and num2 and returns whether num1 is divisible by num2.
 
+// isDivisible(4, 2) // => true
+// isDivisible(9, 3) // => true
+// isDivisible(15, 4) // => false
+
+function isDivisable( num1, num2){
+    if (num1 % num2 == 0){
+        return true;
+        } else {
+            return false
+    }
+}
+const answer1 = isDivisable(4, 2)
+console.log(answer1)
+const answer2 = isDivisable(9, 3)
+console.log(answer2)
+const answer3 = isDivisable(15, 4)
+console.log(answer3)
+//*
