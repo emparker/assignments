c5ba4da301be8922e942931f515d0f129defcf2f
Emily Parker <emilyarparker@gmail.com>
2020-07-23 23:27:48 -0600

    getting there

--- colossal-adventure-rpg/app.js
+++ colossal-adventure-rpg/app.js
@@ -50,7 +50,7 @@ while( isAlive && !hasWon ){
 }
 
 function healthStatus(){
-    hiker.hydration + 1;
+    hiker.hydration + 5;
     console.log(`Your health: ${hiker.health}%`)
 }
 
@@ -66,53 +66,59 @@ function printInventory(){
 
 function hike(){
     const random = Math.floor(Math.random() * 4);
-    console.log(random);
-    hiker.hydration - 3;
+    //console.log(random);
+    hiker.hydration - 5;
     if (random === 0){
         hiker.inventory.push(`SCORE! You found a gold coin from Dusty Rifile's fresh loot!`);
         console.log(`Hike On ${hiker.name}`)
     } else if (random === 1){
         hiker.inventory.push(`YUM! Snacks!`);
         gains()
-        }else if (random === 2){
+    }else if (random === 2){
         console.log('There is defintely something out there, be careful...')
-        //ADD OPTION TO FIGHT?
         enemyAttack();
-        }
+    }
 }
 
 
 
 function enemyAttack(){ 
-    let battle = true;
-    let random = Math.floor(Math.random() * enemies.length);
-    let enemy = enemies[random];
-    console.log(`Oh no! ${enemy}!!!!`)
-        while(battle === true){
-        let random5 = Math.floor(Math.random() * 5);
-        if (random5 === 0)
-            enemyDamageReport();
-            playerDamageReport();
-        if(random5 === 1){    
-                lostItem = [];
-                const lostInventory = hiker.inventory.pop();
-                const lost = lostItem.push(lostInventory);
-                console.log(`Wow ${name} that was impressive! There has been however the unfortunate loss of an item from your inventory. 
-                In the curfulffle you have lost ${lost}`)
-            } else if (random5 === 2){
-                enemy.strength -= 20;
-                enemy.energy -= 20;
-                gains();
-            } else if (random5 === 3){
-                enemies = [];
-                battle = false;
-                enemyParished();
-            } else (enemy.strength === 0 && enemy.energy === 0);{
-                battle = false;
-                playerParished();
-                }
-    }
+    // let battle = true;
+    // while(battle){ 
+    const bailOrFight = readline.keyIn(`This could get ugly, wannna [b]bail or stay and [f]fight?`, {limit: 'bf'});
+    if (bailOrFight === 'b'){
+        bail();
+        //battle = false;
+    } else if (bailOrFight === 'f'){
+        fight();
+    }   
 }
+//}
+
+function bail(){
+    console.log(`Samrt move, ${hiker.name}. Check your Inventory ;)`)
+    gains()
+}
+
+function fight(){
+    let random = Math.floor(Math.random() * enemies.length);
+    let badGuy = enemies[random]; //BAD GUY UNDEFINED OR {OBJECT, OBJECT}
+    console.log(`Oh no! ${badGuy}!!!!`)
+    let random5 = Math.floor(Math.random() * 5);
+    if (random5 === 0){
+        youWin();
+        // theyWin();
+    } else if(random5 === 1){    
+        lostItem = [];
+        const lostInventory = hiker.inventory.pop();
+        const lost = lostItem.push(lostInventory);
+        console.log(`You just barely escaped, well done! However there seems to have been an unfortunate loss to your inventory. 
+        Looks like in the curfulffle you lost ${lost}`)
+    } else if (random5 === 2){
+        theyWin();
+    } 
+}
+
 
 function gains(){
     let gainsRandom = Math.floor(Math.random() * 2);
@@ -133,7 +139,7 @@ function playerDamageReport(){
             
 }
 
-function enemyDamageReport(){
+function enemyDamageReport(){  // NOT QUITE WORKING, ALSO IS IN RIGHT PLACE?
     enemies.energy -= 50;
     enemies.strength -= 50;
     console.log(`${enemies} Damage Report: 
@@ -141,19 +147,32 @@ function enemyDamageReport(){
             Strength: ${enemies.strength}`)
 }
 
-function playerParished(){
+
+function theyWin(){
     if (hiker.health <= 0 && hiker.hydration <= 0){
             console.log(`Sorry to say it ${hiker}, but ya toast... GAME OVER`)
             isAlive = false;
-    } 
-}
-function enemyParished(){
-    if (enemies.strength <= 0 && enemies.energy <= 0){
-            console.log(`YOU JUST CLEANED HOUSE! YOU WIN THE GAME ${name}!!`)
-            hasWon= true;
+    } else {
+        playerDamageRerport();
+        enemyDamageReport();
     }
+    return action
+}
+function youWin(){
+    if (enemies.strength <= 0 && enemies.energy <= 0){
+            console.log(`You survived the Wild West, CONGRATULATIONS ${hiker}!`)
+            hasWon= true;
+    } else {
+        playerDamageReport();
+        enemyDamageReport()
+    }
+    return action
 }
 
+// function playerWins(){
+//     console.log(`You survived the Wild West, CONGRATULATIONS ${hiker}!`);
+//     hasWon = true 
+// }
 
 // const selectItem = realine.keyIn('How about you select an item from your pack! [w] for Water, [p] to use Pepper Spray, [f] for your first aid kit', {limit: 'wpf'})
 // if (selectItem === 'w'){
@@ -173,4 +192,4 @@ function enemyParished(){
 // }
 // function replenishHealth(){
 //     //?
-// }
+
