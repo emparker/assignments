dc1aa9151508054830f3e0e37ba89e467e6a12c5
Emily Parker <emilyarparker@gmail.com>
2020-08-11 18:10:36 -0600

    about half way through

--- array-reduce-exercises/app.js
+++ array-reduce-exercises/app.js
@@ -14,9 +14,39 @@ function total(arr) {
 
 function stringConcat(arr) {
     return arr.reduce(function(final, num){
-        num.concat
-        return final
+        return final += num
+        //final += num.join()
+
     }, "")
+    
 }
 
  console.log(stringConcat([1,2,3])); // "123"
+
+
+// 3) Turn an array of voter objects into a count of how many people voted
+
+function totalVotes(arr) {
+    return arr.reduce(function(final, voter){
+        if (voter.voted){
+            final ++
+        }
+        return final
+    }, 0)
+}
+
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+ console.log(totalVotes(voters)); // 7
