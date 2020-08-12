a72248cdeb5dd2f529b2e3a0fa7ff6fc66244676
Emily Parker <emilyarparker@gmail.com>
2020-08-11 22:36:14 -0600

    close but not done

--- array-reduce-exercises/app.js
+++ array-reduce-exercises/app.js
@@ -50,3 +50,83 @@ var voters = [
     {name: 'Zack', age: 19, voted: false}
 ];
  console.log(totalVotes(voters)); // 7
+
+//4) Given an array of all your wishlist items, figure out how much it would cost to just buy everything at once
+
+function shoppingSpree(arr) {
+    return arr.reduce(function(final, item){
+        final += item.price++
+        return final
+    }, 0)
+}
+
+var wishlist = [
+    { title: "Tesla Model S", price: 90000 },
+    { title: "4 carat diamond ring", price: 45000 },
+    { title: "Fancy hacky Sack", price: 5 },
+    { title: "Gold fidgit spinner", price: 2000 },
+    { title: "A second Tesla Model S", price: 90000 }
+];
+
+ console.log(shoppingSpree(wishlist)); // 227005
+
+
+// 5) Given an array of arrays, flatten them into a single array
+
+function flatten(arr) {
+    return arr.reduce(function(final, flatten){
+        final += flatten.flat()
+        return final
+    }, [])
+}
+//arr.reduce((acc, val) => acc.concat(val), []);
+var arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+ console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
+
+
+// 6) Given an array of potential voters, return an object representing the results of the vote
+
+// Include how many of the potential voters were in the ages 18-25, how many from 26-35, how many from 36-55, and how many of each of those age ranges actually voted. The resulting object containing this data should have 6 properties. See the example output at the bottom.
+
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+
+function voterResults(arr) {
+    return arr.reduce(function(final, stats){
+        if(stats.age >= 18 && stats.age <= 25){
+            return `youngVotes: ${stats.age} midVotes: ${stats.age} oldVotes: ${stats.age}`
+        } 
+        return final
+    }, {})
+}
+
+console.log(voterResults(voters)); // Returned value shown below:
+
+/*
+{ youngVotes: 1,
+    youth: 4,
+    midVotes: 3,
+    mids: 4,
+    oldVotes: 3,
+    olds: 4 
+}
+*/
+
+
