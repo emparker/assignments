17b4d497058d15b3edad450a1d48cbaad5c83aa1
Emily Parker <emilyarparker@gmail.com>
2020-08-19 21:59:42 -0600

    finished

--- catch-me-if-you-can/app.js
+++ catch-me-if-you-can/app.js
@@ -1,15 +1,45 @@
 // 1a) Write a function that returns the sum of two numbers. Throw an error if either argument is not of the data type number:
 try{
     function sum(x, y){
-    return x + y;
+        //check data types first and throw error
+        if (typeof(x) !== "number" || typeof(y) !== "number"){
+            throw new Error("must use a number")
+        } else {
+            return x + y
+            }
     }
-    throw new Error("this is an error message")
-}
-catch(err){
-    if(1 || 2 !== number)
-    console.log(err)
-}
-finally{
-    console.log("im running no matter what")
+    console.log(sum(1, 10))
+    //console something wrong?
+    console.log(sum("1", "2"))
 }
 // 1b) Call the sum function inside a try block using "1" and "2" as arguments. Use console.log within a catch block to inform the user of the error.
+catch(err){
+    console.log("ERROR")
+}
+finally{
+    console.log("I'm running no matter what")
+}
+
+
+// 2a) Given a user object, write a function called login that takes a username and password as parameters. Throw an error if either of them don't match. Otherwise, log to the console a message saying "login successful!"
+
+
+let user = {username: "sam", password: "123abc"};
+try{
+function login(username, password){
+        //check credentials
+        if(username !== user.username || password !== user.password){
+            throw new Error("login not successful")
+        } else {
+            return "login successful"
+        }
+    }
+    console.log(login("sam", "123abc"))
+    console.log(login("james", "876lkj"))
+}
+catch(err){
+    console.log("access denied")
+}
+finally{
+    console.log("why am I here??")
+}
