7519d81802a6ca80c42e718e2679d11b2b34780a
Emily Parker <emilyarparker@gmail.com>
2020-09-03 15:50:56 -0600

    almost complete

--- es6-practice/app.js
+++ es6-practice/app.js
@@ -23,31 +23,89 @@ function runForLoop(pets) {
     return petObjects
 }
 
-// runForLoop(["cat", "dog"])
+runForLoop(["cat", "dog"])
 
 
 // Task 1
 //  Re-write this .map() using an arrow function:
 
 //  Be aware that if JavaScript sees a { directly after the => it will think it's starting a function, and not starting an object, so the : will be an unexpected symbol.
-//  const carrots = ["bright orange", "ripe", "rotten"]
-// const carrots = ["bright orange", "ripe", "rotten"]
+
+const carrots = ["bright orange", "ripe", "rotten"]
 
 //  function mapVegetables(arr) {
 //      return arr.map(function(carrot) {
 //          return { type: "carrot", name: carrot }
 //      })
 //  }
-const carrots = ["bright orange", "ripe", "rotten"]
-
-// function mapVegetables(arr) {
-//     return arr.map(function(carrot) {
-//         return { type: "carrot", name: carrot }
-//     })
-// }
-
+//ES6:
 function mapVegetables(arr) {
     return arr.map((carrot) =>  { return { type: "carrot", name: carrot } })
 }
-
 console.log(mapVegetables(carrots))
+
+// Task 2
+//Re-write this .filter() using an arrow function:
+
+const people = [
+    {
+        name: "Princess Peach",
+        friendly: false
+    },
+    {
+        name: "Luigi",
+        friendly: true
+    },
+    {
+        name: "Mario",
+        friendly: true
+    },
+    {
+        name: "Bowser",
+        friendly: false
+    }
+]
+// function filterForFriendly(arr) {
+//     return arr.filter(function(person) {
+//         return person.friendly
+//     })
+// }
+//ES6:
+function filterForFriendly(arr) {
+    return arr.filter(person => {return person.friendly})
+}
+console.log(filterForFriendly(people))
+
+// Task 3
+// Re-write the following functions to be arrow functions:
+//1.
+// function doMathSum(a, b) {
+//     return a + b
+// }
+doMathSum = (a, b) => a + b
+console.log(doMathSum(3, 3))
+//2.
+// var produceProduct = function(a, b) {
+//     return a * b
+// }
+var produceProduct = (a, b) => a * b
+console.log(produceProduct(2, 10))
+
+// Task 4
+// Write a printString function that takes firstName, lastName, and age as parameters and returns a string like the following: Hi Kat Stark, how does it feel to be 40?
+// firstName should default to "Jane"
+// lastName should default to "Doe"
+// age should default to 100
+//Use Template Literals!
+function Person(firstName, lastName, age){
+    this.jane = firstName;
+    this.doe = lastName;
+    this.age = age;
+}
+Person.prototype.printToString = function() {
+    console.log(`Hi ${this.jane} ${this.doe}, how does it feel to be ${this.age}?`)
+} 
+
+const kat = new Person("kat", "Stark", 40)
+
+kat.printToString()
