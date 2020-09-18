c6b5f0740237d6439ea4e4c264a83a2ef13206f7
Emily Parker <emilyarparker@gmail.com>
2020-09-17 22:23:36 -0600

    basically done with princess in another castle just need it to end

--- princess-in-another-castle/app.js
+++ princess-in-another-castle/app.js
@@ -1,18 +1,14 @@
-const readline = require("readline-sync")
-console.log("This is Princess In Another Castle")
-
-//gameActive boolean
-
+console.log("This is Princess In Another Castle") 
 
 //create a class for a player
 class Player{
-    constructor(name, totalCoins, status, hasStar){
+    constructor(totalCoins = 0, name = "Mario", status = "Small", hasStar = false){
         this.name = name
         this.totalCoins = totalCoins
-        this.status = status
+        // this.status = status
         this.hasStar = hasStar
         this.gameActive = true 
-        this.playerStatus = ["Powered Up", "Big", "Small", "Dead"]
+        this.playerStatus = status
     }
 
 // setName function
@@ -20,31 +16,44 @@ class Player{
         this.name = namePicked
     }
 
-    gotHit(){ //need a for loop?
-            this.status = playerStatus
-            for (let i = 0; i < playerStatus.length; i++){
-                if( playerStatus[i] === "Dead"){
-                    return gameActive = false
-                        
-            }
+    gotHit(){
+        if (mario.hasStar === true){
+            console.log("You lost your star ** sad face **")
+            mario.hasStar = false
+            //return
         }
+        if (this.playerStatus === "power up"){
+            console.log("you got hit and made you BIG")
+            this.playerStatus = "big"
+        } if (this.playerStatus === "big"){
+            console.log("doh you got hit and now SMALL")
+            this.playerStatus = "small"
+        } if (this.playerStatus === "small"){
+            console.log("Ugh a hit you are DEAD")
+            this.playerStatus = "dead"
+        } else (this.playerStatus === "dead")
+            console.log("Game Over")
+            this.gameActive = false
+        
+            
     }
-
+    
 //gotPowerup function
     gotPowerup(){
-            return starActive()
-            // if (playerStatus === "PoweredUp"){
-            //     return starActive()
-            // }
-        }
-
-    // starActive(){
-    //     this.hasStar = true
-    // } 
+            if (this.playerStatus === "PowerUp"){
+                console.log("You got s STAR! Woop woop")
+                return starActive()
+            } if (this.playerStatus === "big"){
+                console.log("A POWER UP! Now you are POWERED UP")
+                this.status = "power up"
+            } else (this.playerStatus === "small")
+                console.log("A POWER UP! Now you are BIG")
+                this.playerStatus = "big"
+    }
 
 //addCoin function
     addCoin(){
-        this.totalCoins = this.totalCoins++
+        this.totalCoins++
     }
     
 //hasStar is active of type boolean?
@@ -54,30 +63,58 @@ class Player{
 
 //print funciton 
     print(){
-        let {name, totalCoins, hasStar, playerStatus} = this
-
-        console.log(name, totalCoins, playerStatus[0], hasStar)
-    }
-
+        let {name, totalCoins, playerStatus} = this
         
-}
+        if(mario.hasStar === true){
+        console.log(`Name: ${name} \n Coins: ${totalCoins} \n Status: ${playerStatus} \n You have a STAR`)
+        } else (mario.hasStar != true)
+            console.log(`Name: ${name} \n Coins: ${totalCoins} \n Status: ${playerStatus}`)
+    }
+}     
 
+//use class to create object 
+const mario = new Player()
+mario.setName("Mario")
 
-//use class to create object ?!?!?  What object?
-const theMarioObject = new Player("Mario", 0, "Small", false)
-console.log(theMarioObject)
-const theLuigiObject = new Player("Luigi", 0, "Small", false)
-console.log(theLuigiObject)
-console.log(setName(theMarioObject))
 // Create a random range function that returns either 0, 1, or 2.
 
+function randomNumber() {
+    return Math.floor(Math.random() * 3)
+}
+
+// Now put the random range function inside a setInterval function that ends after gameActive === false??
 // If the value is 0 call the gotHit() function on the object.
 // If the value is 1 call the gotPowerup() function on the object
 // If the value is 2 call the addCoin() function
+//dont forget to clear interval!
+let gameOn = setInterval(function(){
+    if(mario.gameActive = true){
+        if(randomNumber() === 0){
+            mario.gotHit()
+        } if(randomNumber() === 1){
+            mario.gotPowerup()
+        } else{
+            mario.addCoin()
+        }
+        // Then call the print method on the object.
+        mario.print()
+    } else {
+        clearInterval(gameOn)
+        // Player.gameActive = false how do I end the game?!
+    }
+},3000)
 
-// Then call the print method on the object.
 
-// Now put the random range function inside a setInterval function that ends after gameActive === false
+
+
+
+
+
+
+
+
+
+
 
 
 
