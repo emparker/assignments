66e0df6122dc913127118e30bd1c3a434608f7e0
Emily Parker <emilyarparker@gmail.com>
2020-09-18 21:58:45 -0600

    finished princess in another castle

--- princess-in-another-castle/app.js
+++ princess-in-another-castle/app.js
@@ -5,7 +5,6 @@ class Player{
     constructor(totalCoins = 0, name = "Mario", status = "Small", hasStar = false){
         this.name = name
         this.totalCoins = totalCoins
-        // this.status = status
         this.hasStar = hasStar
         this.gameActive = true 
         this.playerStatus = status
@@ -15,12 +14,11 @@ class Player{
     setName(namePicked){
         this.name = namePicked
     }
-
+//set gotHit funciton
     gotHit(){
         if (mario.hasStar === true){
             console.log("You lost your star ** sad face **")
             mario.hasStar = false
-            //return
         }
         if (this.playerStatus === "power up"){
             console.log("you got hit and made you BIG")
@@ -29,26 +27,26 @@ class Player{
             console.log("doh you got hit and now SMALL")
             this.playerStatus = "small"
         } if (this.playerStatus === "small"){
-            console.log("Ugh a hit you are DEAD")
             this.playerStatus = "dead"
-        } else (this.playerStatus === "dead")
-            console.log("Game Over")
-            this.gameActive = false
-        
             
+            console.log("Game Over")
+            
+            this.gameActive = false 
+            
+        }
     }
     
 //gotPowerup function
     gotPowerup(){
             if (this.playerStatus === "PowerUp"){
+                starActive()
                 console.log("You got s STAR! Woop woop")
-                return starActive()
             } if (this.playerStatus === "big"){
-                console.log("A POWER UP! Now you are POWERED UP")
                 this.status = "power up"
+                console.log("A POWER UP! Now you are POWERED UP")
             } else (this.playerStatus === "small")
-                console.log("A POWER UP! Now you are BIG")
-                this.playerStatus = "big"
+            this.playerStatus = "big"
+            console.log("A POWER UP! Now you are BIG")
     }
 
 //addCoin function
@@ -64,11 +62,11 @@ class Player{
 //print funciton 
     print(){
         let {name, totalCoins, playerStatus} = this
-        
-        if(mario.hasStar === true){
+
+        if (playerStatus === "dead"){
+            console.log(`Name: ${name} \n Final Coin Count: ${totalCoins} \n Well played ${name} Come back soon!`)
+        } else
         console.log(`Name: ${name} \n Coins: ${totalCoins} \n Status: ${playerStatus} \n You have a STAR`)
-        } else (mario.hasStar != true)
-            console.log(`Name: ${name} \n Coins: ${totalCoins} \n Status: ${playerStatus}`)
     }
 }     
 
@@ -77,31 +75,29 @@ const mario = new Player()
 mario.setName("Mario")
 
 // Create a random range function that returns either 0, 1, or 2.
-
 function randomNumber() {
     return Math.floor(Math.random() * 3)
 }
 
-// Now put the random range function inside a setInterval function that ends after gameActive === false??
+// Now put the random range function inside a setInterval function
 // If the value is 0 call the gotHit() function on the object.
 // If the value is 1 call the gotPowerup() function on the object
 // If the value is 2 call the addCoin() function
 //dont forget to clear interval!
 let gameOn = setInterval(function(){
-    if(mario.gameActive = true){
+    if(mario.gameActive == true){
         if(randomNumber() === 0){
             mario.gotHit()
         } if(randomNumber() === 1){
             mario.gotPowerup()
-        } else{
+        } else if(randomNumber() === 2){
             mario.addCoin()
         }
         // Then call the print method on the object.
         mario.print()
-    } else {
+    } else 
         clearInterval(gameOn)
-        // Player.gameActive = false how do I end the game?!
-    }
+    
 },3000)
 
 
