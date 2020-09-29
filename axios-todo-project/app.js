affc0c44f33862554cccdd7173fb112df2830089
Emily Parker <emilyarparker@gmail.com>
2020-09-29 16:15:40 -0600

    axios is underway

--- axios-todo-project/app.js
+++ axios-todo-project/app.js
@@ -1,1 +1,41 @@
-console.log(axios)  
+//console.log(axios)  
+
+//get todos from API
+function getData(){
+    axios.get("https://api.vschool.io/emily/todo")
+    .then(result => toDoData(result.data))
+    .catch(err => console.log(err))
+}
+
+const toDoList = document.getElementById('todo-list')
+
+//List TODOs to the DOM
+function toDoData(data){
+    for(let i = 0; i < data.length; i++){
+
+        // const container = document.createElement('div')
+        const h1 = document.createElement('h1')
+        const p = document.createElement('p')
+        const img = document.createElement('img')
+        img.className = 'img-container'
+        img.style.backgroundImage = data[i].imgUrl
+        p.textContent = data[i].description
+        h1.textContent = data[i].title
+        const urls = data[i].imgUrl
+        console.log(urls)
+        
+
+        toDoList.appendChild(h1)
+        toDoList.appendChild(p)
+        toDoList.appendChild(img)
+        // container.appendChild(toDoList)
+        //console.log(data[i])
+    }
+}
+
+getData()
+
+
+
+//form for post request
+const todoForm = document["todo-form"]
