4543b0a350774c9768efadc27cae5f1e74bb84fc
Emily Parker <emilyarparker@gmail.com>
2020-09-30 13:11:09 -0600

    lots of progress needs more work

--- axios-todo-project/app.js
+++ axios-todo-project/app.js
@@ -3,7 +3,7 @@
 //get todos from API
 function getData(){
     axios.get("https://api.vschool.io/emily/todo")
-    .then(result => toDoData(result.data))
+    .then(response => toDoData(response.data))
     .catch(err => console.log(err))
 }
 
@@ -12,26 +12,54 @@ const toDoList = document.getElementById('todo-list')
 //List TODOs to the DOM
 function toDoData(data){
     for(let i = 0; i < data.length; i++){
-
-        // const container = document.createElement('div')
-        const h1 = document.createElement('h1')
-        const p = document.createElement('p')
-        const img = document.createElement('img')
-        img.className = 'img-container'
-        img.style.backgroundImage = data[i].imgUrl
-        p.textContent = data[i].description
-        h1.textContent = data[i].title
-        const urls = data[i].imgUrl
-        console.log(urls)
-        
-
-        toDoList.appendChild(h1)
-        toDoList.appendChild(p)
-        toDoList.appendChild(img)
-        // container.appendChild(toDoList)
-        //console.log(data[i])
+        createToDo(data[i])
     }
 }
+
+function createToDo(todoItem){
+
+    const h1 = document.createElement('h1')
+    const p = document.createElement('p')
+    const p2 = document.createElement('p')
+    const img = document.createElement('img')
+    const input = document.createElement('input')
+    img.setAttribute("src", todoItem.imgUrl)
+    img.setAttribute("height", "100px")
+    img.setAttribute("width", "100px")
+    input.setAttribute("type", "checkbox")
+    // checked?
+    p.textContent = todoItem.description
+    p2.textContent = todoItem.price
+    h1.textContent = todoItem.title
+    
+    toDoList.appendChild(h1)
+    toDoList.appendChild(p)
+    toDoList.appendChild(p2)
+    toDoList.appendChild(img)
+    toDoList.appendChild(input)
+    
+}
+
+const toDoForm = document["todo-form"] 
+
+toDoForm.addEventListener("submit", e => {
+    e.preventDefault()
+
+    const toDoObj = {
+        description : toDoForm.description.value,
+        price : toDoForm.price.value,
+        title : toDoForm.title.value,
+        imgUrl : toDoForm.image.value
+    }
+
+    console.log(toDoObj)
+    axios.post("https://api.vschool.io/emily/todo", toDoObj)
+        .then(response => console.log("success"))
+        .catch(error => console.log(error))
+
+
+})
+
 
 getData()
 
