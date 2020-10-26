82455cd438ab8169457800faa1b375867c9747cc
Emily Parker <emilyarparker@gmail.com>
2020-10-26 15:50:06 -0600

    after some collaberation made a few changes and notes

--- rolling-dice-react-state/src/Dice.js
+++ rolling-dice-react-state/src/Dice.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 function Dice(props) {
+    console.log("props", props)
     return (
         <div>
             <h1>Dice Roll: {props.number}</h1>
--- rolling-dice-react-state/src/DiceBox.js
+++ rolling-dice-react-state/src/DiceBox.js
@@ -1,57 +1,61 @@
-import React, {Component} from "react"
-import Dice from "./Dice"
+import React, { Component } from "react";
+import Dice from "./Dice";
 
-class DiceBox extends Component { 
+class DiceBox extends Component {
     constructor() {
-        super()
+        super();
         this.state = {
-            num1: 0,
-            num2: 0,
-            num3: 0,
-            num4: 0,
-            num5: 0,
-            isChecked: false
-        }
-        this.rollDice = this.rollDice.bind(this)
+        num1: 0,
+        num2: 0,
+        num3: 0,
+        num4: 0,
+        num5: 0,
+        // isChecked: false
+        };
+        this.rollDice = this.rollDice.bind(this);
     }
-    
-    
 
     rollDice() {
+    for (const num in this.state) {
+        // console.log( num )
         this.setState({
-            num1: Math.floor(Math.random() * 6),
-            num2: Math.floor(Math.random() * 6),
-            num3: Math.floor(Math.random() * 6),
-            num4: Math.floor(Math.random() * 6),
-            num5: Math.floor(Math.random() * 6)
+            [num]:Math.floor(Math.random() * 6)
         })
 
-    
-    }
-
-    render(){
-        
-        return (
-            <div>
-                {/* <h1>Dice Roll 1: {this.state.num1}</h1>
-                <h1>Dice Roll 2: {this.state.num2}</h1>
-                <h1>Dice Roll 3: {this.state.num3}</h1>
-                <h1>Dice Roll 4: {this.state.num4}</h1>
-                <h1>Dice Roll 5: {this.state.num5}</h1> */}
-                <button onClick={this.rollDice}>this is the button</button>
-                <Dice number={this.state.num1}/>
-                <Dice number={this.state.num2}/>
-                <Dice number={this.state.num3}/>
-                <Dice number={this.state.num4}/>
-                <Dice number={this.state.num5}/>
-                
-            </div>
-        ) 
-    }
-    
+    } 
+    //before Danny's enlightening "for in" statement
+    // this.setState({
+    //   num1: Math.floor(Math.random() * 6),
+    //   num2: Math.floor(Math.random() * 6),
+    //   num3: Math.floor(Math.random() * 6),
+    //   num4: Math.floor(Math.random() * 6),
+    //   num5: Math.floor(Math.random() * 6),
+    // });
 }
 
-export default DiceBox
+render() {
+return (
+    <div>
+    {/*     <h1>Dice Roll 1: {this.state.num1}</h1>
+            <h1>Dice Roll 2: {this.state.num2}</h1>
+            <h1>Dice Roll 3: {this.state.num3}</h1>
+            <h1>Dice Roll 4: {this.state.num4}</h1>
+            <h1>Dice Roll 5: {this.state.num5}</h1> */}
+    <button onClick={this.rollDice}>this is the button</button>
+    <Dice number= {this.state.num1} />
+    <Dice number= {this.state.num2} />
+    <Dice number= {this.state.num3} />
+    <Dice number= {this.state.num4} />
+    <Dice number= {this.state.num5} />
+    </div>
+);
+}
+}
+
+export default DiceBox;
 
 
-
+// props = {
+//     number: 38,
+//     name: bob
+// }
