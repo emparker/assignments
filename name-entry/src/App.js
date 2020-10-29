163c784df4e1c28ca5dd6851187369a2495133cc
Emily Parker <emilyarparker@gmail.com>
2020-10-28 23:44:07 -0600

    name entry complete

--- name-entry/src/App.js
+++ name-entry/src/App.js
@@ -7,24 +7,32 @@ class App extends Component {
             name:"",
             list: ["Dr Pepper", "G. H Creators", "Cap 'N Crunch"]
         }
-        this.handleChange = this.handleChange.bind(this)
+        // this.handleChange = this.handleChange.bind(this)
     }
 
-    handleChange(event) {
-        const { name, value } = event.target
-        const arr = this.state.list
-        arr.push(this.state.person)
-        //push input value to prevState?
+    handleChange = (event) => {
+        const { name, value} = event.target
         this.setState({ 
-            [name]: value, 
-            // [list]: value 
+            [name]: value
         })
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.setState(prevState => {
+            return { 
+                name:"",
+                list: [prevState.name, ...prevState.list] 
+            }
+        }) 
+    }
+
     render() {
+        
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
+                    
                     <input 
                         type="text" 
                         placeholder="Enter Name"
@@ -37,13 +45,14 @@ class App extends Component {
                     <br />
 
                     <button>Submit</button>
+                    
                 </form>
-                
+
                 <h1>INFO: {this.state.name} </h1>
                 <br />
 
-                <ol onChange={this.handleChange}>
-                    {this.state.list.map(person => (<li key={this.state.name}>{person}</li>))}
+                <ol>
+                    {this.state.list.map((person, i) => (<li key={person + i}>{person}</li>))}
                 </ol>
                 
             </div>
