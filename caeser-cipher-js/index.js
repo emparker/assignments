fb082fda2b44113569f9039ad0ec7f187bf2b9e5
Emily Parker <emilyarparker@gmail.com>
2020-11-11 17:18:37 -0700

    almost complete. works just need to account for special characters

--- caeser-cipher-js/index.js
+++ caeser-cipher-js/index.js
@@ -9,18 +9,36 @@ const theBet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 
 //use the input variable and the shift variable to decode.  duh
 //users input needs to change the chosen words letters, to the shifted letter per a specified number of characters using theBet array as a guide, then display encoded word array/string?
+const specialChar = ' \\\' "(){}!@#$%^&*()1234567890-_=+|":;<>,.?/~`'
 
-const shiftedText = () => {
+shiftedText = () => {
     for (let i = 0; i < input.length; i++){
         const place = theBet.indexOf(input[i]) + shift
-        console.log(place)
-        if (place <= 26 ){
-            arr.push(place)
-            
+        if (specialChar.includes(place[i])){
+            arr.push(place[i])
+            continue
+            //place is an index!!
+            // console.log(place)
         }
-    }
-}
+            if (place <= 26 ){
+                arr.push(theBet[place])
+                
+            } else if (place >= 26){
+                arr.push(theBet[place - 26])
+            }
+        }
+        console.log(arr)
+            
+        
+    // return the final string
+    // return arr.join("")  could have made this simpler... 
+    return arr.reduce((finalString, currentLetter) => {
+        return finalString = `${finalString}${currentLetter}`
+    }, "")
+    
 
+}
+console.log(arr)
 console.log(shiftedText())
 // console.log("the input", input)
 // console.log("the shift", shift)
@@ -28,3 +46,31 @@ console.log(shiftedText())
 //test for the indexOf on theBet string
 // const theE = theBet.indexOf("e")
 // console.log("the e", theE)
+
+
+// reduce method
+// arr.reduce(callbackFn, initalizedValue, index)
+// arr.reduce()
+
+// ['d', 'g', 'f']
+// finalString = ''
+
+// 1st iteration 
+// currentLetter = 'd'
+// finalString= finalString+currentLetter
+// finalString= ''+'d'
+// finalString='d'
+
+// // 2nd iteration
+// currentLetter = 'g'
+// finalString = finalString+currentLetter
+// finalString = 'd'+'g'
+// finalString = 'dg'
+
+// //3rd iteration 
+// currentLetter = 'f'
+// finalString = finalString+currentLetter
+// finalString = 'dg' + 'f'
+// finalString = 'dgf'
+
+// final String: 'dgf'
