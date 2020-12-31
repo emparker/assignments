dbf8f71fb927ab0cce5e683e2707d6f137aacd54
Emily Parker <emilyarparker@gmail.com>
2020-12-31 12:09:01 -0700

    Ugly Things complete

--- ugly-things/src/components/UglyThing.js
+++ ugly-things/src/components/UglyThing.js
@@ -1,58 +1,79 @@
-import React from "react"
-import { UglyContextConsumer } from "../uglyContext"
+import React, {useState} from "react"
+import {UglyContextConsumer} from "../uglyContext"
 
 function UglyThing(props) {
-    // console.log(props)
-    const {title, imgUrl, description, _id} = props.uglyData
+  const [showModal,
+    setShowModal] = useState(false);
+  // console.log(props)
+  const {title, imgUrl, description, _id} = props.uglyData
 
-    const handleModClick = (e) => {
+//   const handleModClick = (e) => {
 
-        let element = e.target.parentElement
-        element.classList.add("hidden")
+//     let element = e.target.parentElement
+//     element
+//       .classList
+//       .add("hidden")
 
-    }
+//   }
 
+    //toggles a boolean value
+    // const toggle =()=>{
+    //     setShowModal(prev => !prev)
+    // }
+  return (
+    <UglyContextConsumer>
+      {({
+        deleteThing,
+        submitEditedThing,
+        modalTitle,
+        modalDescription,
+        modalWindow,
+        handleModalChange
+      }) => (
+        <div className="ugly-div">
+          <h2>{title}</h2>
+          <p>{description}</p>
+          <img src={imgUrl} alt={description}/>
+          <br/>
+          <button onClick={() => deleteThing(_id)}>Delete</button>
+          <br/>
+          <button
+            onClick={() => {
+                setShowModal(true)
+                console.log(showModal)
+                modalWindow(title, description)
+          }}>Edit</button>
+          {showModal
+            ? <div className="modal-div">
+                <form
+                  onSubmit={(e) => {
+                    e.preventDefault()
+                    submitEditedThing(e, _id, modalTitle, modalDescription)
+                    setShowModal(false)
+                }}>
+                  <h1>Edit Me</h1>
+                  <br/>
+                  <input
+                    value={modalTitle}
+                    placeholder="Title"
+                    name="modalTitle"
+                    onChange={handleModalChange}/>
+                  <br/>
+                  <input
+                    value={modalDescription}
+                    placeholder={"description"}
+                    name="modalDescription"
+                    onChange={handleModalChange}/>
+                  <br/>
+                  <button>Save</button>
+                </form>
+              </div>
+            : null}
+        </div>
+      )}
+    </UglyContextConsumer>
 
-    return (
-        <UglyContextConsumer>
-            {({deleteThing, submitEditedThing, modalTitle, modalDescription, modalWindow, handleModalChange}) =>(
-                <div className="ugly-div">
-                    <h2>{title}</h2>
-                    <p>{description}</p>
-                    <img src={imgUrl} alt={description} />
-                    <br/>
-                    <button onClick={() => deleteThing(_id)}>Delete</button>
-                    <br/>
-                    <button onClick={(e) => {
-                        e.target.parentElement.lastChild.classList.toggle("hidden")
-                        modalWindow(title, description)}}>Edit</button>
-                    <div className="hidden modal-div">
-                        <form onSubmit={(e) => submitEditedThing(e, _id, modalTitle, modalDescription)}>
-                            <h1>Edit Me</h1>
-                            <br/>
-                            <input 
-                            value={modalTitle}
-                            placeholder="Title"
-                            name="modalTitle"
-                            onChange={handleModalChange}
-                            />
-                            <br/>
-                            <input 
-                            value={modalDescription}
-                            placeholder={"description"}
-                            name="modalDescription"
-                            onChange={handleModalChange}
-                            />
-                            <br/>
-                            <button onClick={handleModClick}>Save</button>
-                        </form>
-                    </div>
-                </div>
-            )}
-        </UglyContextConsumer>
-        
-    )
+  )
 }
 
 export default UglyThing
-
