ace72aeeb16430d6ee7af9ebd78307d016755f55
Emily Parker <emilyarparker@gmail.com>
2021-02-05 22:42:49 -0700

    css up, landing page done, move to form page

--- react-api-project/src/pages/LandingPage.js
+++ react-api-project/src/pages/LandingPage.js
@@ -4,18 +4,25 @@ import { Link } from "react-router-dom"
 export default function LandingPage() {
 
     return (
-        <div>
-            <h1>DOGCAT</h1>
-
-            <Link className="link" to={{
-                pathname: "/form",
-                state: { type : "dog" }
-            }}>Woof</Link>
+        <>
+            <h1 className="header">DOGCAT</h1>
+            <div className="div-links">
+                <div className="div-dog">
+                    <Link className="link-dog" to={{
+                        pathname: "/form",
+                        state: { type : "dog" }
+                    }}>Woof</Link>
+                </div>
+                <h3 className="or">OR</h3>
+                <div className="div-cat">
+                    <Link className="link-cat" to={{
+                        pathname: "/form",
+                        state: { type : "cat" } 
+                    }}>Meow</Link>
+                </div>
+            </div>
             
-            <Link className="link" to={{
-                pathname: "/form",
-                state: { type : "cat" } 
-            }}>Meow</Link>
-        </div>
+            
+        </>
     )
 }
