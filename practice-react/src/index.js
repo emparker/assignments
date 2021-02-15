f7269a7816e010cd16d5b0830f1e5aa88672f7a2
Emily Parker <emilyarparker@gmail.com>
2021-02-15 12:26:21 -0700

    practice up

---
+++ practice-react/src/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+
+ReactDOM.render(
+    <App />,
+    document.getElementById("root")
+)
+
+
+// const { sum, divide } = require("./math.js")
+
+// console.log(sum)
+// console.log(divide)
+
+
