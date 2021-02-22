157ac17b8997c32cdbf907f5e168b841ee690eab
Emily Parker <emilyarparker@gmail.com>
2021-02-22 14:12:09 -0700

    thing-finder complete with extra credit

--- thing-finder/fruitRouter.js
+++ thing-finder/fruitRouter.js
@@ -1,57 +1,80 @@
-const express = require ("express")
-const fruitRouter = express.Router()
-const uuid = require("uuid").v4
-
+const express = require("express");
+const fruitRouter = express.Router();
+const uuid = require("uuid").v4;
 
 const inventoryItems = [
     {
         name: "banana",
         type: "food",
         price: 200,
-        _id: uuid()
-    },{
+        _id: uuid(),
+    },
+    {
         name: "pants",
         type: "clothing",
         price: 2500,
-        _id: uuid()
-    },{
+        _id: uuid(),
+    },
+    {
         name: "basket ball",
         type: "toy",
         price: 1000,
-        _id: uuid()
-    },{
+        _id: uuid(),
+    },
+    {
         name: "rockem sockem robots",
         type: "toy",
         price: 1500,
-        _id: uuid()
-    },{
+        _id: uuid(),
+    },
+    {
         name: "shirt",
         type: "clothing",
         price: 800,
-        _id: uuid()
-    },{
+        _id: uuid(),
+    },
+    {
         name: "soup",
         type: "food",
         price: 300,
-        _id: uuid()
-    },{
+        _id: uuid(),
+    },
+    {
         name: "flour",
         type: "food",
         price: 100,
-        _id: uuid()
+        _id: uuid(),
+    },
+];
+
+fruitRouter.get("/", (req, res) => {
+    if (!req.query.type) {
+        return res.send(inventoryItems);
+    } else {
+        const filteredItems = inventoryItems.filter((item) => {
+        return item.type === req.query.type;
+        });
+        res.send(filteredItems);
     }
-]
+});
 
-fruitRouter.get("/", (req, res) => {
-    res.send(inventoryItems)
-})
+//filter by max and min price
+fruitRouter.get("/price", (req, res) => {
+    let { lowPrice, highPrice } = req.query;
 
-fruitRouter.get("/", (req, res) => {
-    console.log(req.query)
-    const filteredItems = inventoryItems.filter(item => {
-        return item.type === req.query.type ///what goes here first, the return??!
-    })
-    res.send(filteredItems)
-})
+    if (lowPrice === undefined) {
+        lowPrice = 0;
+    }
+    // console.log("low", lowPrice);
+    if (highPrice === undefined) {
+        highPrice = 10000000;
+    }
+    // console.log("high", highPrice);
+    
+    const filteredPrice = inventoryItems.filter((item) => {
+        return item.price >= lowPrice && item.price <= highPrice;
+    });
+    res.send(filteredPrice);
+});
 
-module.exports = fruitRouter
+module.exports = fruitRouter;
