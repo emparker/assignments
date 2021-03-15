e1d26e0cbe3b847af25789d63e5fb13570f85790
Emily Parker <emilyarparker@gmail.com>
2021-03-14 22:58:27 -0600

    update done and crud store complete

--- crud-store/routes/inventoryRouter.js
+++ crud-store/routes/inventoryRouter.js
@@ -25,6 +25,47 @@ inventoryRouter.post("/", (req, res, next) => {
     })
 })
 
+//update one(PUT)//
+inventoryRouter.put("/:itemId", (req, res, next) => {
+    Inventory.findOneAndUpdate(
+        { _id: req.params.itemId },
+        req.body,  //can use our own object to updat with here. 
+        { new : true },  //sends back updated version 
+        (err, updatedItem) => {
+            if(err) {
+                res.status(500)
+                return next(err)
+            }
+            return res.status(201).send(updatedItem)
+        }
+    )
+})
+
+//get one//
+inventoryRouter.get("/:itemId", (req, res, next) => {
+    Inventory.findOne({ _id: req.params.itemId },
+    (err, foundItem) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(foundItem)
+    })
+})
+
+//delete//
+inventoryRouter.delete("/:itemId", (req, res, next) => {
+    Inventory.findOneAndDelete(
+        { _id: req.params.itemId },
+        (err, deletedItem) => {
+            if(err){
+                res.status(500)
+                return next(err)
+            }
+            return res.status(200).send(`You have successfully deleted an item from inventory: ${deletedItem.item}`)
+        }
+    )
+})
 
 
 module.exports = inventoryRouter
