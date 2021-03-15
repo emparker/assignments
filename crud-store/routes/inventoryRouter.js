0e8349dd6f568825a31936d8a003ffad7aff9e8a
Emily Parker <emilyarparker@gmail.com>
2021-03-14 21:57:16 -0600

    put request

--- crud-store/routes/inventoryRouter.js
+++ crud-store/routes/inventoryRouter.js
@@ -2,6 +2,7 @@ const express = require('express')
 const inventoryRouter = express.Router()
 const Inventory = require("../models/inventoryModel.js")
 
+//get all//
 inventoryRouter.get("/", (req, res, next) => {
     Inventory.find((err, items) => {
         if(err){
@@ -11,5 +12,19 @@ inventoryRouter.get("/", (req, res, next) => {
         return res.status(200).send(items)
     })
 })
+
+//create one//
+inventoryRouter.post("/", (req, res, next) => {
+    const newItem = new Inventory(req.body)
+    newItem.save((err, savedItem) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(savedItem)
+    })
+})
+
+
 
 module.exports = inventoryRouter
