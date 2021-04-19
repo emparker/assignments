7111a83ef51c6958c5bad07893f163568b0ae016
Emily Parker <emilyarparker@gmail.com>
2021-04-19 16:42:18 -0600

    all backend routes complete

--- sign-up-login-practice/routes/todoRouter.js
+++ sign-up-login-practice/routes/todoRouter.js
@@ -13,8 +13,20 @@ todoRouter.get("/", (req, res, next) => {
     })
 })
 
+//get todos by user id
+todoRouter.get("/user", (req, res, next) => {
+    Todo.find({ user: req.user._id }, (err, todos) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(todos)
+    })
+})
+
 //add new todo
 todoRouter.post("/", (req, res, next) => {
+    req.body.user = req.user._id
     const newTodo = new Todo(req.body)
     newTodo.save((err, savedTodo) => {
         if(err){
@@ -28,7 +40,7 @@ todoRouter.post("/", (req, res, next) => {
 //delete todo
 todoRouter.delete("/:todoId", (req, res, next) => {
     Todo.findOneAndDelete(
-        { _id: req.params.todoId },
+        { _id: req.params.todoId, user: req.user._id },
         (err, deletedTodo) => {
             if(err){
             res.status(500)
@@ -42,7 +54,7 @@ todoRouter.delete("/:todoId", (req, res, next) => {
 //update todo
 todoRouter.put("/:todoId", (req, res, next) => {
     Todo.findOneAndUpdate(
-        { _id: req.params.todoId },
+        { _id: req.params.todoId, user: req.user._id },
         req.body,
         { new: true },
         (err, updatedTodo) => {
