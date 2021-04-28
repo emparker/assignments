048396688bdbdf292920492621caa9231eb293ea
Emily Parker <emilyarparker@gmail.com>
2021-04-27 19:18:25 -0600

    completed

---
+++ aclymate-csv-parser/index.js
@@ -0,0 +1,19 @@
+const { rawData } = require("./metaData.js")
+
+
+
+
+const formatCsv = (data) => {
+    let formattedData = []
+    const headers = data[0]
+    for(let i = 1; i < data.length; i++){
+        let info = data[i]
+        let obj = {}
+        for(let j = 0; j < info.length; j++){
+            obj[headers[j]] = info[j]
+        }
+        formattedData.push(obj)
+    }
+    console.log(formattedData)
+}
+formatCsv(rawData)
